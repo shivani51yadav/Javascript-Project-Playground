2760627f4fade88e3849bde737ce7dd391dc5f37
shivani51yadav <shivani51yadav@gmail.com>
2023-11-18 14:52:44 +0530

    adiing counter

---
+++ counter/app.js
@@ -0,0 +1,29 @@
+
+let count = 0;
+
+const value = document.querySelector("#value");
+const btn = document.querySelectorAll(".btn");
+
+btn.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count == 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
