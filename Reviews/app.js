b00d37e6e59a138a4db3c223ae0dbd0420b38c27
shivani51yadav <shivani51yadav@gmail.com>
2023-11-21 22:25:32 +0530

    added review

---
+++ Reviews/app.js
@@ -0,0 +1,87 @@
+const reviews=[
+    {
+        id:1,
+        name:"susan smith",
+        job:"web developer",
+        img: 'https://www.course-api.com/images/people/person-1.jpeg',
+        text:"I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+    },
+    {
+        id:2,
+        name:"anna johnson",
+        job:"web designer",
+        img: 'https://www.course-api.com/images/people/person-2.jpeg',
+        text:"Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated",
+    },
+    {
+        id:3,
+        name:"peter jones",
+        job:"intern",
+        img: 'https://www.course-api.com/images/people/person-3.jpeg',
+        text:"Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo",
+    },
+    {
+        id:4,
+        name:"bill anderson",
+        job:"the boss",
+        img: 'https://www.course-api.com/images/people/person-4.jpeg',
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    },
+];
+
+// select items 
+const img=document.getElementById('person-img');
+const author=document.getElementById('author');
+const job=document.getElementById('job');
+const info=document.getElementById('info');
+
+const prevBtn=document.querySelector('.prev-btn');
+const nextBtn=document.querySelector('.next-btn');
+const randomBtn=document.querySelector('.random-btn');
+
+// set starting item
+let currentItem=0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded',function(){
+   const item=reviews[currentItem];
+    img.src=item.img;
+    author.textContent=item.name;
+    job.textContent=item.job;
+    info.textContent=item.text;
+});
+
+// show person based on item
+function showPerson(person){
+    const item=reviews[person];
+    img.src=item.img;
+    author.textContent=item.name;
+    job.textContent=item.job;
+    info.textContent=item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click',function(){
+    currentItem++;
+    if(currentItem>reviews.length-1){
+        currentItem=0;
+    }
+    showPerson(currentItem);
+});
+
+// show prev person
+prevBtn.addEventListener('click',function(){
+    currentItem--;
+    if(currentItem<0){
+        currentItem=reviews.length-1;
+    }
+    showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener('click',function(){
+    currentItem=Math.floor(Math.random()*reviews.length);
+    showPerson(currentItem);
+});
+
+
