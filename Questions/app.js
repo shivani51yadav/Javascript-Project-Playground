ca0054f3057ae3ef33b061656488b9f32a23ecde
shivani51yadav <shivani51yadav@gmail.com>
2023-11-23 18:32:05 +0530

    complete the questions project

---
+++ Questions/app.js
@@ -0,0 +1,32 @@
+// const questions = document.querySelectorAll(".question");   
+
+// questions.forEach(function(question){
+//     const btn=document.querySelector(".question-btn");  
+//     btn.addEventListener("click",function(){
+//         questions.forEach(function(item){
+//             if(item!==question){
+//                 item.classList.remove("show-text");
+//             }
+//         });
+//         question.classList.toggle("show-text");
+//     });
+// });
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
