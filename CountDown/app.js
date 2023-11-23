0e5dd90611d2b239115ddaa5a040c46c597761d5
shivani51yadav <shivani51yadav@gmail.com>
2023-11-23 18:10:50 +0530

    countDown completed

---
+++ CountDown/app.js
@@ -0,0 +1,26 @@
+const endDate = "20 November 2023 12:00 PM";
+
+document.getElementById("end-date").innerText = endDate;
+const input = document.querySelectorAll("input");
+function clock() {
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
+
+  // if(diff < 0) return;
+
+  //convert into days
+  input[0].value = Math.floor(diff / 3600 / 24);
+  //convert into hours
+  input[1].value = Math.floor(diff / 3600) % 24;
+  //convert into minutes
+  input[2].value = Math.floor(diff / 60) % 60;
+  //convert into seconds
+  input[3].value = Math.floor(diff) % 60;
+}
+
+clock();
+
+setInterval(() => {
+  clock();
+}, 1000);
